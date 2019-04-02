0fd2587403b0b7c087863feeb7156dd0ecdd670f
Alexander Ananchenko <snvtr@mail.ru>
2019-04-02 20:58:23 +0300

    init pycut.py

--- lmp3thw/pycut.py
+++ lmp3thw/pycut.py
@@ -7,13 +7,23 @@ import os, sys, argparse
 
 ### __defs__ ###
 
+def parse_chars(arg_str):
+    return char_pattern
 
+def parse_fields(arg_str):
+    return field_pattern
+
+def cut_chars(char_pattern):
+    pass
+    
+def cut_fields(field_pattern):
+    pass
 
 ### __main__ ###
 
 parser = argparse.ArgumentParser(description='lmt3thw-ex8: pygrep.py')
 parser.add_argument('-d', '--delimiter', help='delimeter', nargs=1, action='store')
-parser.add_argument('-c', '--characters', help='characters to print out: like x1,x2 or x1-x3,x4- etc', nargs=1, action='store', default='0')
+parser.add_argument('-c', '--characters', help='characters to print out: like c1,c2 or c1-c3,c4- etc', nargs=1, action='store', default='0')
 parser.add_argument('-f', '--fields', help='fields to print out: like f1,f3,f4- or f3-f5,f7 etc', nargs=1, action='store', default='0')
 parser.add_argument('filename', help='filename to cut', action='store')
 args = parser.parse_args()
