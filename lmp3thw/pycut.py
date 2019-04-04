ec8fe724015c811688cae5773e300470b0a8a465
Alexander Ananchenko <snvtr@mail.ru>
2019-04-04 12:34:23 +0300

    finished pycut.py

--- lmp3thw/pycut.py
+++ lmp3thw/pycut.py
@@ -11,52 +11,73 @@ def parse_cut_params(arg_str):
     """ заполняет массив для полей которые будут вырезаться """
     Temp0 = [0] * 255
     Temp1 = arg_str.split(',')
-#    print('debug. split by comma:', Temp1)
+    
+    if len(Temp1) <= 1:
+        Temp0[int(Temp1[0])] = 1
+        return Temp0
+
     for temp1 in Temp1:
-        Temp2 = temp1.split('-')
-#        print('debug. split by hyphen:', Temp2)
-        if Temp2[1] == '':
-            Temp2[1] = '254'
-        for i in range(int(Temp2[0]), int(Temp2[1])+1):
-            Temp0[i] = 1
+        if temp1.find('-') >= 0:
+            Temp2 = temp1.split('-')
+            if Temp2[1] == '':
+                Temp2[1] = '254'
+            for i in range(int(Temp2[0]), int(Temp2[1])+1):
+                Temp0[i] = 1
+        else:
+            Temp0[int(temp1[0])] = 1
     return Temp0
 
 def cut_chars(Chars, filename):
     """ для каждой строки из файла собираем результирующую строку по маске из chars """
-#    try:
+# хорошо было бы тут ловить экспешн на открытии файла
+
     cut_line = []
+
     with open(filename, 'r') as f:
+
         for line in f:
+
             cut_line[:] = []
+
             if len(line.rstrip()) > 254:
                 line_len = 254
             else:
                 line_len = len(line.rstrip())
+
             for char_idx in range(line_len):
                 if Chars[char_idx+1] == 1:
-                    print('Char %r is added to the list' % line[char_idx])
                     cut_line.append(line[char_idx:char_idx+1])
-            print('debug. joined cut string:', ''.join(cut_line), '\n')
- #   except:
- #       print('Cannot open file %s' % filename)
- #       sys.exit(1)
+
+            print(''.join(cut_line)) # result for each line
 
     return True
 
 def cut_fields(Fields, delim, filename):
     """ для каждой строки из файла собираем поля порезанные по delim и списку полученных полей из fields """
-    try:
-        with open(filename,'r') as f:
-            for line in f:
-                pass # проверяем каждый символ и собираем строку
-    except:
-        print('Cannot open the file %r' % filename)
-        sys.exit(1)
+
+    cut_line = []
+
+    print('debug. delimeter is: %s.' % delim)
+
+    with open(filename,'r') as f:
+
+        for line in f:
+
+            cut_line[:] = []
+
+            Temp0 = line.split(delim)
+            for idx in range(1,len(Fields)): # min(len(Fields), len(Temp0)) 
+                if Fields[idx] == 1:
+                    if len(Temp0) >= idx:
+                        cut_line.append(Temp0[idx-1])
+
+            print(''.join(cut_line)) # result for each line 
 
     return True
+
 ### __main__ ###
 
-parser = argparse.ArgumentParser(description='lmt3thw-ex8: pygrep.py')
+parser = argparse.ArgumentParser(description='lmt3thw-ex9: pycut.py')
 parser.add_argument('-d', '--delimiter', help='delimeter', nargs='+', action='store')
 group = parser.add_mutually_exclusive_group(required=True)
 group.add_argument('-c', '--characters', help='characters to print out: like c1,c2 or c1-c3,c4- etc', nargs=1, action='store', default='-1')
@@ -67,11 +88,9 @@ args = parser.parse_args()
 # fill the Characters/Fields with values:
 print('debug:',vars(args))
 
-if args.characters == -1:
+if args.characters == '-1':
     Filter = parse_cut_params(args.fields[0])
-    print('debug:', Filter)
-    cut_fields(Filter, args.delimiter, args.filename)
+    cut_fields(Filter, args.delimiter[0].replace("'",''), args.filename)
 else:
     Filter = parse_cut_params(args.characters[0])
-    print('debug:', Filter)
     cut_chars(Filter, args.filename)
