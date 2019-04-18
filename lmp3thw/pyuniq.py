67ef3eb32d1d17c99cf492da4ee7587e8429ad5a
Alexander Ananchenko <snvtr@mail.ru>
2019-04-18 16:58:07 +0300

    Update pyuniq.py

--- lmp3thw/pyuniq.py
+++ lmp3thw/pyuniq.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
 # Implements some uniq features, default uniq feature and -c option
+# 'functionized'
 
 import os, sys
 import argparse
@@ -13,6 +14,44 @@ def random_string(length=20):
     letters = string.ascii_letters
     return ''.join(random.choice(letters) for i in range(length))
 
+def pyuniq(args):
+
+    try:
+        if args.FILE != '-':
+            uniq_f = open(args.FILE,'r')
+        else:
+            uniq_f = sys.stdin
+    except:
+        print('Cannot open file %s. Exiting.' % args.FILE)
+
+    random_str = random_string()
+#    print(vars(args), random_str)
+
+    prev = random_str
+    cnt  = 1
+    output = ''
+
+    for line in uniq_f:
+        line = line.rstrip()
+        if line == prev:
+            cnt += 1
+        else:
+            if args.count and prev != random_str:
+                output += '%d %s\n' % (cnt, prev)
+            elif prev != random_str:
+                output += '%s\n' % prev
+            cnt = 1
+            prev = line
+
+    if args.count:
+        output += '%d %s\n' % (cnt, prev)
+    else:
+        output += '%s\n' % prev
+
+    uniq_f.close()
+
+    return output
+
 ### __main__ ###
 
 parser = argparse.ArgumentParser(description='for training purposes')
@@ -21,35 +60,5 @@ parser.add_argument('-u', '--uniq', help='uniq default behaviour, displays uniqu
 parser.add_argument('FILE', help='File to process, "-" means stdin', action='store')
 args = parser.parse_args()
 
-try:
-    if args.FILE != '-':
-        uniq_f = open(args.FILE,'r')
-    else:
-        uniq_f = sys.stdin
-except:
-    print('Cannot open file %s. Exiting.' % args.FILE)
-
-random_str = random_string()
-print(vars(args), random_str)
-
-prev = random_str
-cnt  = 1
-
-for line in uniq_f:
-    line = line.rstrip()
-    if line == prev:
-        cnt += 1
-    else:
-        if args.count and prev != random_str:
-            print('%d %s' % (cnt, prev))
-        elif prev != random_str:
-            print('%s' % prev)
-        cnt = 1
-        prev = line
-
-if args.count:
-    print('%d %s' % (cnt, prev))
-else:
-    print('%s' % prev)
-
-uniq_f.close()
+if __name__ == '__main__':
+    print(pyuniq(args))
